add970b6e333d55d1599ef8cc600992f9cbc49a9
Maximyma <76785012+Maximyma@users.noreply.github.com>
2021-01-22 17:25:46 +0200

    Update tasks-03.js

--- tasks-03.js
+++ tasks-03.js
@@ -3,4 +3,7 @@ class SmallestIntegerFinder {
   findSmallestInt(args) {
     return Math.min(...args)
   }
+}https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  return circle.radius * 2 * Math.PI
 }
